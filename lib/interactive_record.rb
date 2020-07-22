7aabe8b32b6da00c338bd1fb9dad5bbcd039ee06
Jamal Rjoul <jamensky1991@gmail.com>
2020-07-22 17:01:48 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,17 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+     self.name.downcase.pluralize
+  end
+
+  def self.column_names
+     sql <<= SQL
+        PRAGMA table_info(table_name)
+     SQL
+  end
+
 end
